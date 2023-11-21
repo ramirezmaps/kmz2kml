e46f839e8f2bc4a898776e403426754ec828ce84
RamirezMaps <ramirezign@gmail.com>
2023-11-21 20:10:09 -0300

    Update kmztokml2.py
    renombrado

--- kmztokml2.py
+++ kmztokml2.py
@@ -10,18 +10,18 @@ def kmz_to_kml(kmz_content):
         return kml_content
 
 def main():
-    st.title("KMZ 2 KML ")
+    st.title("KMZ 2 KML JIA ")
 
     uploaded_file = st.file_uploader("Cargar archivo KMZ", type=["kmz"])
 
     if uploaded_file is not None:
-        st.write("File uploaded successfully!")
+        st.write("Archivo cargado exitosamente!")
 
         if st.button("Convertir"):
             kmz_content = uploaded_file.read()
             kml_content = kmz_to_kml(kmz_content)
 
-            # Download the converted KML file
+         
             st.download_button(
                 label="Descargar KML",
                 data=kml_content,
